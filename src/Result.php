b6ec60f5be8a6b12e41e75218101045e56e4eca1
Tao Pan <pantao@yoloyolo.tv>
2016-11-26 18:40:32 +0800

    Add SDK

---
+++ src/Result.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * 服务器返回的结果.
+ * @author wangkuiwei
+ * @name Result
+ * @desc Sender发送消息后，服务器返回的结果。
+ */
+namespace Pantaovay\XMPush;
+
+class Result
+{
+    private $errorCode;
+    private $raw;
+
+    public function __construct($jsonStr){
+        $data = json_decode($jsonStr,true);
+        $this->raw = $data;
+        $this->errorCode = $data['code'];
+    }
+
+    public function getErrorCode(){
+        return $this->errorCode;
+    }
+
+    public function getRaw(){
+        return $this->raw;
+    }
+
+    public function getReason(){
+        return $this->raw['reason'];
+    }
+}
